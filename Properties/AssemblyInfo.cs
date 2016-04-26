3ab32d08eb7b4f1f9003970984934788e94f4c21
Phil <phil@aquilatech.com>
2016-04-26 17:02:12 -0400

    Release 1.1.0
    Switched installer to Inno Setup.
    64 bit install now uses correct folder.
    Application is now signed.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WOLAgent")]
 [assembly: AssemblyDescription("Aquila WOL Agent")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Aquila Technology")]
 [assembly: AssemblyProduct("Agent")]
-[assembly: AssemblyCopyright("Copyright (c) 2004 - 2014, Aquila Technology, LLC.")]
+[assembly: AssemblyCopyright("Copyright (c) 2004 - 2016, Aquila Technology, LLC.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
